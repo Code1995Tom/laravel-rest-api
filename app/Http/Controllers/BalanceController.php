e6c549b642fe3c3ecdb52807825d7aa7a887d874
Tomas <tomas.e.aguilera95@gmail.com>
2021-02-05 15:11:18 -0300

    add GET balance endponint for non-existing accounts

--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Account;
 
 class BalanceController extends Controller
 {
-    //
+    public function show(Request $request)
+    {
+        $accountId = $request->input('account_Id');
+        $account = Account::findOrFail($accountId);
+        return $account->balance;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::post('/reset', [ResetController::class, 'reset']);
-Route::get('/balance', [BalanceController::class, 'show']);
+Route::get('/balance', [App\Http\Controllers\BalanceController::class,'show']);
 Route::post('/event', [EventController::class, 'store']);
