a795d0f7bd6a7ad3e144b31b635312db432842f9
Tomas <tomas.e.aguilera95@gmail.com>
2021-02-05 15:44:11 -0300

    add event endpoint to create an account with an initial balance

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Account;
 
 class EventController extends Controller
 {
-    //
+    //POST /event {"type":"deposit", "destination":"100", "amount":10}
+    //201 {"destination": {"id":"100", "balance":10}}
+
+    public function store(Request $request)
+    {
+        //Event::create(...);
+        if ($request->input('type') === 'deposit') {
+            return $this->deposit(
+                $request->input('destination'),
+                $request->input('amount')
+            );
+        }
+    }
+
+    private function deposit($destination, $amount)
+    {
+        $account = Account::firstOrCreate([
+            'id' => $destination
+        ]);
+
+        $account->balance += $amount;
+        $account->save();
+
+        return response()->json([
+            'destination' => [
+                'id' => $account->id,
+                'balance' => $account->balance
+            ]
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/reset', [ResetController::class, 'reset']);
 Route::get('/balance', [App\Http\Controllers\BalanceController::class,'show']);
-Route::post('/event', [EventController::class, 'store']);
+Route::post('/event', [App\Http\Controllers\EventController::class, 'store']);
