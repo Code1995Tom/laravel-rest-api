dc33acb9372d6fc8cf1ea57f9378cb1e3cde856f
Tomas <tomas.e.aguilera95@gmail.com>
2021-02-06 21:17:44 -0300

    add reset method

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Artisan;
 
 class ResetController extends Controller
 {
-    //
+    public function reset()
+    {
+        Artisan::call('migrate:fresh');
+
+        return 'OK'; //Status code is 200
+    }
 }
